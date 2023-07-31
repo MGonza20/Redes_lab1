0d5d9a86e9f61daddf65f85d12800c74b3a8c906
Sara Paguaga <64711979+MGonza20@users.noreply.github.com>
2023-07-30 19:32:55 -0600

    comments

--- hamming_encoder.py
+++ hamming_encoder.py
@@ -15,10 +15,14 @@ def encode(n: int, frame: str) -> str:
     guide = matrix_[0]
     for col_indx, row in enumerate(matrix_[1:len(matrix_) - 1], start=1):
         for row_indx, _ in enumerate(row):
+            # Se convierten los numeros de cada columna para poder asignar los bits 
+            # correspondientes
             binary_num = bin(row_indx+1)[2:]
             if abs(col_indx) <= len(binary_num):
-                if int(binary_num[-col_indx]) == 1:
+                if binary_num[-col_indx] == '1':
                     selected_num = guide[row_indx]
+                    # Se asigna el numero correspondiente de la trama si no 
+                    # es una columna para asignar a un bit de paridad
                     if selected_num != 'p':
                         row[row_indx] = selected_num    
     return matrix_
