82dcb67095c1d37a73df1eac460a14d12225636f
Sara Paguaga <64711979+MGonza20@users.noreply.github.com>
2023-07-30 19:22:57 -0600

    Generating matrix - Hamming

---
+++ hamming_encoder.py
@@ -0,0 +1,29 @@
+
+
+def power_of_two(n):
+    return (n & (n - 1)) == 0
+
+def encode(n: int, frame: str) -> str:
+    m = len(frame)
+    r = n - m
+    matrix_ = [['p' if power_of_two(col) else 'd' for col in range(1, n+1)] 
+               for row in range(r+2)]
+    
+    iter_f = iter(frame)
+    matrix_[0] = [next(iter_f) if x == 'd' else x for x in matrix_[0]]
+    
+    guide = matrix_[0]
+    for col_indx, row in enumerate(matrix_[1:len(matrix_) - 1], start=1):
+        for row_indx, _ in enumerate(row):
+            binary_num = bin(row_indx+1)[2:]
+            if abs(col_indx) <= len(binary_num):
+                if int(binary_num[-col_indx]) == 1:
+                    selected_num = guide[row_indx]
+                    if selected_num != 'p':
+                        row[row_indx] = selected_num    
+    return matrix_
+
+
+
+a = encode(11, '0101001')
+uu = 123
